e4eebb9757ced51928ce8b950dd58c08f7b5eaaa
Zun Wang <zunwang@microsoft.com>
2024-05-18 00:29:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(name='deephe3',
           'pymatgen', 
           'tqdm', 
           'numpy', 
-          'torch==1.13.1',  # to support mace
+          'torch>=1.12',  # to support mace
           'torch-scatter', 
           'torch-sparse', 
           'torch-cluster', 
           'torch-spline-conv', 
-          'torch-geometric', 
+          'torch-geometric==2.3.0', 
           'e3nn==0.4.4',  # to support mace
           'h5py', 
           'tensorboard', 
